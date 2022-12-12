77a2d08b09ae1b3ac7540474725f196c541f0e21
Tobias Tran <tobias_tran@msn.com>
2022-12-12 14:49:28 +0100

    Solution for Day 6 - Part 1

---
+++ Solutions/Day-06/DaySix.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode_2022.Solutions
+{
+    public class DaySix : ISolution
+    {
+        public void Run()
+        {
+            var dataPacket = File.ReadAllText("InputFiles\\Day-06-Input.txt");
+
+            var queue = new Queue<char>();
+
+            var markerIndex = 0;
+            for(var i=0; i<dataPacket.Length; i++)
+            {
+                var c = dataPacket[i];
+
+                queue.Enqueue(c);
+
+                if (queue.Count > 4)
+                    queue.Dequeue();
+
+                if (queue.Count == 4 && MarkerFound(queue))
+                {
+                    markerIndex = i+1;
+                    break;
+                }
+            }
+
+            Console.WriteLine(markerIndex);
+        }
+
+        public void RunPartTwo()
+        {
+            // TODO
+        }
+
+        public bool MarkerFound(Queue<char> queue)
+        {
+            HashSet<char> chars = new HashSet<char>();
+            foreach(var c in queue)
+            {
+                if (chars.Contains(c))
+                    return false;
+
+                chars.Add(c);
+            }
+
+            return true;
+        }
+    }
+}
